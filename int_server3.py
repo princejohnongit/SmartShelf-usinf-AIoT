b366a124e8cbd2bba1881001bf7ef8c9f58c9ffb
Prince John <125338190+princejohnongit@users.noreply.github.com>
2025-03-19 11:36:06 +0530

    Add files via upload

---
+++ int_server3.py
@@ -0,0 +1,38 @@
+#from flask import Flask, request, jsonify
+
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+#-------------------------------------------------------------------------
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/uploads', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        image_data = request.data
+        if image_data:
+            filename = "image.jpg" #or create a unique filename.
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+            with open(filepath, 'wb') as f:
+                f.write(image_data)
+            
+            return 'Image uploaded successfully!', 200
+        else:
+            return 'No image data received.', 400
+
+#------------------------------------------------------------------------
+
+
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
+
+#--------------------------------------------------------------------------
---
+++ model.py
@@ -0,0 +1,74 @@
+
+import os
+from inference_sdk import InferenceHTTPClient
+import threading
+import time
+# Initialize the inference client
+client = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="eyKFS9MeCHFrf13n7oTH"
+)
+#API Key about Notifications
+api_key = "o.o58jcLa8H18LQwbFf6MRCsiD5qVxK0zS"  # Replace with your actual API key
+#-----------------------------------------------------------
+
+def noti(text):
+    from pushbullet import Pushbullet
+    pb = Pushbullet(api_key)
+    push = pb.push_note("Stock Alert", text)
+    print("Notification sent!")
+#---------------------------------------------------------------
+def loki(alerts):
+            print(alerts)
+            noti(alerts[0])
+            alerts.clear()
+
+
+
+#----------------------------------------------------------
+def mpredict(filepath):
+    # Run inference
+    result = client.run_workflow(
+        workspace_name="smartshelf",
+        workflow_id="custom-workflow-2",
+        images={"image": filepath},
+        use_cache=True
+    )
+    #print("result",result)
+
+    # Process inference results
+    m_threshold = 4  # Example threshold for 'm'
+    s_threshold = 3 # Example threshold for 's'
+    alerts = []
+
+    for item in result:
+        for prediction in item.get('predictions', {}).get('predictions', []):
+            class_str = prediction['class']
+            print(class_str)
+            prefix = class_str[0]
+            number_str = class_str[1:]
+
+            try:
+                number = int(number_str)
+                if prefix == 'm' and number < m_threshold:
+                    alerts.append("Stock below alert for matchbox")
+                    loki(alerts)
+                elif prefix == 's' and number < s_threshold:
+                    alerts.append("Stock below alert for soapbox")
+                    loki(alerts)
+                
+            except ValueError:
+                alerts.append(f"Could not convert '{number_str}' to an integer: {number_str}")
+
+def schedule_mpredict(filepath):
+    for i in range(30):
+        mpredict(filepath)
+        [print("Prediction Ran Once------")]
+
+        time.sleep(10)
+    
+    
+
+if __name__ == '__main__':
+    filepath = "O:\\WORK\\B.tech\\S6\\MiniProject\\Working\\uploads\\image.jpg"
+    schedule_mpredict(filepath)
